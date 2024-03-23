d4d4f7eca0d9baabaae26bf60c05f3db963e02e4
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2024-03-23 19:16:25 +0200

    Add likes service

--- 03-kicks/src/app/core/services/posts.service.ts
+++ 03-kicks/src/app/core/services/posts.service.ts
@@ -30,5 +30,26 @@ export class PostsService {
     const url = `http://localhost:3030/data/sneakers/${postId}`;
     return this.http.put<PostInterface>(url, postData);
   }
+
+  deletePostById(postId: string): Observable<unknown> {
+    const url = `http://localhost:3030/data/sneakers/${postId}`;
+    return this.http.delete<unknown>(url);
+  }
+
+
+  likePostById(postId: string): Observable<unknown> {
+    const url = 'http://localhost:3030/data/likes';
+    return this.http.post<unknown>(url, {postId});
+  }
+
+  canLike(postId: string, userId: string): Observable<number> {
+    const url = `http://localhost:3030/data/likes?where=postId%3D%22${postId}%22%20and%20_ownerId%3D%22${userId}%22&count`;
+    return this.http.get<number>(url);
+  }
+
+  postTotalLikes(postId: string): Observable<number> {
+    const url = `http://localhost:3030/data/likes?where=postId%3D%22${postId}%22&distinct=_ownerId&count`;
+    return this.http.get<number>(url);
+  }
   
 }
