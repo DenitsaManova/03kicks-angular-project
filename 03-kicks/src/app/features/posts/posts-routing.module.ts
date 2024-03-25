1e15bfd1853ed949804eb8541f432038d861cd58
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2024-03-25 13:11:34 +0200

    Add guards

--- 03-kicks/src/app/features/posts/posts-routing.module.ts
+++ 03-kicks/src/app/features/posts/posts-routing.module.ts
@@ -4,6 +4,9 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
 import { DetailsComponent } from './details/details.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { hasUserGuard } from 'src/app/core/guards/has-user.guard';
 
 
 const routes: Routes = [
@@ -13,15 +16,21 @@ const routes: Routes = [
     },
     {
         path: "create",
+        canActivate: [hasUserGuard],
         component: CreateComponent
     },
     {
         path: "sneakers/edit/:sneakerId",
+        canActivate: [hasUserGuard],
         component: EditComponent
     },
     {
         path: "sneakers/details/:sneakerId",
         component: DetailsComponent
+    }, 
+    {
+        path: '**', 
+        component: NotFoundComponent
     }
 ];
 
--- 03-kicks/src/app/features/profile/profile-routing.module.ts
+++ 03-kicks/src/app/features/profile/profile-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { isGuestGuard } from 'src/app/core/guards/is-guest.guard';
 
 const routes: Routes = [
     {
         path: "login",
+        canActivate: [isGuestGuard],
         component: LoginComponent
     },
     {
         path: "register",
+        canActivate: [isGuestGuard],
         component: RegisterComponent
     }
 ];
