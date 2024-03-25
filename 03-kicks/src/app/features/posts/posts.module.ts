ca3029970e4412ac5236dff73390903a26ad3277
Denitsa Manova <122971781+DenitsaManova@users.noreply.github.com>
2024-03-25 13:11:22 +0200

    Add not found compnent in declarations

--- 03-kicks/src/app/features/posts/posts.module.ts
+++ 03-kicks/src/app/features/posts/posts.module.ts
@@ -5,13 +5,14 @@ import { CatalogComponent } from './catalog/catalog.component';
 import { CreateComponent } from './create/create.component';
 import { DetailsComponent } from './details/details.component';
 import { EditComponent } from './edit/edit.component';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './not-found/not-found.component'; 
 
 
 
 
 @NgModule({
-  declarations: [CatalogComponent, CreateComponent, DetailsComponent, EditComponent],
+  declarations: [CatalogComponent, CreateComponent, DetailsComponent, EditComponent, NotFoundComponent],
   imports: [
     CommonModule,
     PostsRoutingModule,
